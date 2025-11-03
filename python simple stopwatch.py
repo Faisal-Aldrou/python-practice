ecd63714c7df5bfc2218a169f7ac89fa90a57e66
Faisal-Aldrou <faisalaldrou5@gmail.com>
2025-11-03 16:37:28 +0300

    Add files via upload

---
+++ python simple stopwatch.py
@@ -0,0 +1,7 @@
+import time
+start_timer = time.time()
+input("press enter to stop the timer...")
+end_timer = time.time()
+timer_duration = end_timer - start_timer
+print(f"timer duration: {timer_duration} seconds")
+
